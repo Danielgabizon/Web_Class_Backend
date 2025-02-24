16a935f687cf049b8600a8951ca4e1e2c147ba9c
Daniel <daniel.gabizon1105@gmail.com>
2025-02-24 14:51:35 +0200

    changed app config

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import initApp from "./server";
 const port = process.env.PORT;
-
-initApp()
-  .then((app) => {
+(async () => {
+  try {
+    const app = await initApp();
     app.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`);
     });
-  })
-  .catch((error) => {
+  } catch (error) {
     console.error("Failed to initialize app:", error);
-  });
+  }
+})();
